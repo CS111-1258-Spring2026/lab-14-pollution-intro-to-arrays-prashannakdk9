5b1eaf1fcce0642f430c0a3b372c4e8408a4a854
prashannakdk9 <prashannakhadka09@gmail.com>
2026-05-21 04:45:07 +0000

    change

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// YOUR NAME:
+// YOUR NAME:Prashanna khadka
 // COLLABORATORS:
 // LAST MODIFIED DATE: 
 /*
@@ -33,22 +33,55 @@ class Main
   public static void main(String[] args) 
   {
 		// DECLARATION SECTION
+    double[] co2Levels = {
+      371.32, 373.45, 375.98, 377.70, 379.98,
+      382.09, 384.03, 385.83, 387.64, 390.10,
+      391.85, 394.06, 396.74, 398.87, 401.01,
+      404.41, 406.76, 408.72, 411.66, 414.24
+    };
+    int[] years = new int[20];
+    double growth;
 
 		// INITIALIZATION SECTION
+    for(int i = 0; i < years.length; i++)
+    {
+      years[i] = 2001 + i;
+    }
 
 		// INPUT SECTION 
 
 		// PROCESSING SECTION
+    growth = co2Levels[co2Levels.length - 1] - co2Levels[0];
 
 		// OUTPUT SECTION
   
-    //Explanation of oil drum units
-    System.out.println("\nData displayed for each year begins at 360 ppm.\nEach additional oil drum (🛢 ) represents an additional 10 ppm.\n");
-
     //Column titles of graph
-    System.out.print("Year");
-    UtilityBelt.printCentered(55,"CO₂ in Atmosphere (ppm)");
+    System.out.println("Year                CO₂ in Atmosphere (ppm)");
 
+    printGraph(co2Levels, years);
+    System.out.printf("%nFrom 2001 to 2020, the average atmospheric CO₂ levels across the globe has grown %.2f ppm.%n", growth);
+
+  }
+
+  public static void printBar(double value)
+  {
+    int drums = (int)(value - 360);
+
+    for(int i = 0; i < drums; i++)
+    {
+      System.out.print("🛢");
+    }
+
+    System.out.printf(" %.2f%n", value);
+  }
+
+  public static void printGraph(double[] co2Levels, int[] years)
+  {
+    for(int i = 0; i < co2Levels.length; i++)
+    {
+      System.out.print(years[i] + " ");
+      printBar(co2Levels[i]);
+    }
   }
 
 }
